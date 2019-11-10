9bb721f55f04b2a638fc7f39b41411ed42077151
Scorphe <Scorphe@163.com>
2019-11-10 13:14:32 +0800

    T4_reConstructBinaryTree Done!

---
+++ T4_reConstructBinaryTree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+import java.util.Arrays;
+public class T4_reConstructBinaryTree {
+    public static void main(String[] args) {
+        int [] pre = {2,1,4,7,3,5,6,8};
+        int [] in =  {4,7,1,2,5,3,8,6};
+        TreeNode root =  reConstructBinaryTree(pre,in);
+        System.out.println(root.val);
+    }
+    public static TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+        if (pre.length == 0 || in.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(pre[0]);
+        // 在中序中找到前序的根
+        for (int i = 0; i < in.length; i++) {
+            if (in[i] == pre[0]) {
+                // 左子树，注意 copyOfRange 函数，左闭右开
+                root.left = reConstructBinaryTree(Arrays.copyOfRange(pre, 1, i + 1), Arrays.copyOfRange(in, 0, i));
+                // 右子树，注意 copyOfRange 函数，左闭右开
+                root.right = reConstructBinaryTree(Arrays.copyOfRange(pre, i + 1, pre.length), Arrays.copyOfRange(in, i + 1, in.length));
+                break;
+            }
+        }
+        return root;
+    }
+} 
