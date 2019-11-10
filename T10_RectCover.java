3848601b43269513372308873cc365dcf7af573d
Scorphe <Scorphe@163.com>
2019-11-10 15:17:41 +0800

    T10_RectCover Done!

---
+++ T10_RectCover.java
@@ -0,0 +1,26 @@
+class T10_RectCover {
+   // 
+    // f(n) = 1 ，(n=1);
+    //      = 2 ,(n=2);
+    //      = 3 ,(n=3);
+    //      = 5 ,(n=4);
+    // ..............;
+    //      = f(m-2) + f(m-1),(n = m)
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println("n = " +  RectCover(n));
+    }
+
+    public static int RectCover(int target) {
+        if (target < 0){
+            return 0;
+        }
+        if(target <= 2){
+            return target;
+        }
+        else{
+            return RectCover(target-1) +  RectCover(target-2);
+        }
+
+    }
+}
